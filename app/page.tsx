e587a39f6a8f8c1b64e00251871e50aec7a441b7
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2024-05-10 19:25:51 +0900

    ボタンの色を変えた

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,11 @@ import { Box, Button, Typography } from "@mui/material";
 
 export default function Home() {
   return (
-    <Box>
-      <Button variant="contained" href="/saki">Contained</Button>
-      <Typography>muiの勉強会</Typography>
-    </Box>
+      <Box>
+        <Button variant="contained" href="/saki" color="inherit">
+          Contained
+        </Button>
+        <Typography>muiの勉強会</Typography>
+      </Box>
   );
 }
